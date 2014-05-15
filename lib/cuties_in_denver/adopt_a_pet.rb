6d41d0ff4812e39b204213e27550f9854bc929ba
Peter W <techieshark@gmail.com>
2014-05-14 18:58:33 -0700

    grab API URL from environment rather than hard coding it

--- lib/cuties_in_denver/adopt_a_pet.rb
+++ lib/cuties_in_denver/adopt_a_pet.rb
@@ -3,7 +3,10 @@ require 'json'
 require 'open-uri'
 
 class AdoptAPet
-  URL = 'http://adopt-a-pet-mesa.herokuapp.com/api'
+  # Now we need to grab the URL of the city-specific API that tells us about available pets
+  # (If the environment doesn't have a $CUTIES_API_URL variable set, we'll assume we're in Denver.)
+  URL =  ENV.fetch('CUTIES_API_URL', 'http://adopt-a-pet-denver.herokuapp.com/api')
+
 
   def self.random
     pet = fetch_pet while pet.nil? || pet.error?
